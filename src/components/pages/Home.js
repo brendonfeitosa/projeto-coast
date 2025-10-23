8c4181ff0dbad050ef6ef2ae584bf5bae9a3f868
Brendon Feitosa <brendonfeitosa@hotmail.com>
2025-10-23 01:03:17 -0300

    Pagina Home criada

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,18 @@
+import styles from "./Home.module.css"
+import savings from '../../img/costs_logo.png'
+import LinkButton from "../layouts/LinkButton"
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.homeContainer}>
+            <h1>
+                Bem-vindo ao <span>Coast</span>
+            </h1>
+            <p>Comece a gerenciar seus projetos agora mesmo!</p>
+            <LinkButton to="/newproject" text="Criar Projeto"/>
+            <img scr={savings} alt="Coast" />
+        </section>
+    )
 }
 
 export default Home
