acaef192656fa9bd97b5fa89661efcec1ecf5fee
Brendon Feitosa <brendonfeitosa@hotmail.com>
2025-10-22 23:39:48 -0300

    Criando o layout basico

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import Company from "./components/pages/Company";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layouts/Container";
+
 function App() {
   return (
-    <div className="App">
-      <p>Coast</p>
-    </div>
+    <Router>
+      <div>      
+        <Link exact to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="minHeight">
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
